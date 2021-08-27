8be5e39aed38a7bf45c88fc1884332ecf7e791d5
boredream <48262906@qq.com>
2021-08-27 16:00:29 +0800

    mysql + jpa

---
+++ src/main/java/com/boredream/springbootdemo/entity/UserInfo.java
@@ -0,0 +1,38 @@
+package com.boredream.springbootdemo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}

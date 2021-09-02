4035fe61e2b6e8fab56d8a179c6d46a5b82ff860
boredream <48262906@qq.com>
2021-09-02 15:15:04 +0800

    jwt token

--- src/main/java/com/boredream/springbootdemo/auth/JwtAuthFilter.java
+++ src/main/java/com/boredream/springbootdemo/auth/JwtAuthFilter.java
@@ -24,10 +24,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtil jwtUtil;
 
-    private String tokenHeader="Authorization";
-
-    private String tokenPrefix="Bearer";
-
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
@@ -35,33 +31,29 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             FilterChain chain) throws ServletException, IOException {
 
         // 从http头部读取jwt
-        String authHeader = request.getHeader(this.tokenHeader);
-        if (authHeader != null && authHeader.startsWith(tokenPrefix)) {
+        String token = request.getHeader("token");
+        String username = null, role = null;
 
-            final String authToken = authHeader.substring(tokenPrefix.length() + 1); // The part after "Bearer "
-            String username = null, role = null;
+        // 从jwt中解出账号与角色信息
+        try {
+            username = jwtUtil.getUsernameFromToken(token);
+            role = jwtUtil.getClaimFromToken(token, "role", String.class);
+        } catch (Exception e) {
+            log.debug("异常详情", e);
+            log.info("无效token");
+        }
 
-            // 从jwt中解出账号与角色信息
-            try {
-                username = jwtUtil.getUsernameFromToken(authToken);
-                role = jwtUtil.getClaimFromToken(authToken, "role", String.class);
-            } catch (Exception e) {
-                log.debug("异常详情", e);
-                log.info("无效token");
-            }
+        // 如果jwt正确解出账号信息，说明是合法用户，设置认证信息，认证通过
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                    username, null, AuthUser.getAuthoritiesByRole(role));
 
-            // 如果jwt正确解出账号信息，说明是合法用户，设置认证信息，认证通过
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                        username, null, AuthUser.getAuthoritiesByRole(role));
+            // 把请求的信息设置到UsernamePasswordAuthenticationToken details对象里面，包括发请求的ip等
+            auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                // 把请求的信息设置到UsernamePasswordAuthenticationToken details对象里面，包括发请求的ip等
-                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            // 设置认证信息
+            SecurityContextHolder.getContext().setAuthentication(auth);
 
-                // 设置认证信息
-                SecurityContextHolder.getContext().setAuthentication(auth);
-
-            }
         }
 
         // 调用下一个过滤器

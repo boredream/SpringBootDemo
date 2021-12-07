be64ebf2ae487dd62c04b4cbae838c744b00ee68
boredream <48262906@qq.com>
2021-12-07 15:00:33 +0800

    CodeGenerator

--- src/main/java/com/boredream/springbootdemo/CodeGenerator.java
+++ src/main/java/com/boredream/springbootdemo/CodeGenerator.java
@@ -1,108 +1,52 @@
 package com.boredream.springbootdemo;
 
-import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
-import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.boredream.springbootdemo.entity.BaseEntity;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.TemplateType;
+
+import java.util.Collections;
 
 public class CodeGenerator {
 
     public static void main(String[] args) {
-        // 代码生成器
-        AutoGenerator mpg = new AutoGenerator();
-
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/src/main/java");
-        gc.setAuthor("boredream");
-        gc.setOpen(false);
-        gc.setSwagger2(true);
-        mpg.setGlobalConfig(gc);
-
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/love_cookbook");
-        // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("root");
-        mpg.setDataSource(dsc);
-
-        // 包配置
-        PackageConfig pc = new PackageConfig();
-//      pc.setModuleName("module");
-        pc.setParent("com.boredream.springbootdemo");
-        mpg.setPackageInfo(pc);
-
-        // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                // to do nothing
-            }
-        };
-
-        // 如果模板引擎是 freemarker
-        // String templatePath = "/templates/mapper.xml.ftl";
-        // 如果模板引擎是 velocity
-        // String templatePath = "/templates/controller2.java.vm";
-
-        // 自定义输出配置
-//        List<FileOutConfig> focList = new ArrayList<>();
-//        // 自定义配置会被优先输出
-//        focList.add(new FileOutConfig(projectPath + "resources/templates/mytemp.java.vm") {
-//            @Override
-//            public String outputFile(TableInfo tableInfo) {
-//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-//                return projectPath + "/aaa.java";
-//            }
-//        });
-//        cfg.setFileCreate(new IFileCreate() {
-//            @Override
-//            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-//                // 判断自定义文件夹是否需要创建
-//                checkDir("调用默认方法创建的目录，自定义目录用");
-//                if (fileType == FileType.MAPPER) {
-//                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
-//                    return !new File(filePath).exists();
-//                }
-//                // 允许生成模板文件
-//                return true;
-//            }
-//        });
-//        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
-
-        // 配置模板
-        TemplateConfig templateConfig = new TemplateConfig();
-
-        // 配置自定义输出模板
-        // 指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-        // templateConfig.setEntity("templates/entity2.java");
-//        templateConfig.setService(null);
-        templateConfig.setController("templates/RestController.java");
-        templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
-
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setNaming(NamingStrategy.underline_to_camel);
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setSuperEntityClass(BaseEntity.class);
-        strategy.setEntityLombokModel(true);
-        strategy.setRestControllerStyle(true);
-        // 公共父类
-//      strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
-        // 写于父类中的公共字段
-        strategy.setSuperEntityColumns("id");
-        strategy.setInclude("feed_back");
-        strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix(pc.getModuleName() + "_");
-        mpg.setStrategy(strategy);
-//      mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
+        // https://baomidou.com/guide/generator-new.html
+        FastAutoGenerator
+                .create("jdbc:mysql://localhost:3306/love_cookbook",
+                        "root",
+                        "root")
+                .globalConfig(builder -> {
+                    builder.author("boredream") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir(System.getProperty("user.dir") + "/src/main/java"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.boredream.springbootdemo") // 设置父包名
+//                            .moduleName("system") // 设置父包模块名
+                            .entity("entity")
+                            .service("service")
+                            .serviceImpl("service.impl")
+                            .mapper("mapper")
+                            .controller("controller")
+                            .other("other")
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml,
+                                    System.getProperty("user.dir") + "/src/main/resources/mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("recommend_todo_group"); // 设置需要生成的表名
+//                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+                .templateConfig(builder -> builder
+                        .disable(TemplateType.XML, TemplateType.CONTROLLER, TemplateType.SERVICE, TemplateType.SERVICEIMPL)
+                        .entity("/templates/entity.java") // 禁用模板
+                        .service("/templates/service.java")
+                        .serviceImpl("/templates/serviceImpl.java")
+                        .mapper("/templates/mapper.java")
+                        .mapperXml("/templates/mapper.xml")
+                        .controller("/templates/controller.java")
+                        .build())
+//                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
     }
 
 }

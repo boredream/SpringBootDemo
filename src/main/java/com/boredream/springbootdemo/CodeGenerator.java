5d8c0db9780dc7521fb092306b44ca7080a07588
boredream <48262906@qq.com>
2021-12-07 17:27:27 +0800

    CodeGenerator

--- src/main/java/com/boredream/springbootdemo/CodeGenerator.java
+++ src/main/java/com/boredream/springbootdemo/CodeGenerator.java
@@ -3,6 +3,7 @@ package com.boredream.springbootdemo;
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.config.TemplateType;
+import com.boredream.springbootdemo.entity.BaseEntity;
 
 import java.util.Collections;
 
@@ -18,6 +19,7 @@ public class CodeGenerator {
                     builder.author("boredream") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
+                            .disableOpenDir() // 禁止打开输出目录
                             .outputDir(System.getProperty("user.dir") + "/src/main/java"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
@@ -33,19 +35,21 @@ public class CodeGenerator {
                                     System.getProperty("user.dir") + "/src/main/resources/mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("recommend_todo_group"); // 设置需要生成的表名
+                    builder.addInclude("recommend_todo", "recommend_the_day", "recommend_todo_group"); // 设置需要生成的表名
 //                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
                 })
                 .templateConfig(builder -> builder
                         .disable(TemplateType.XML, TemplateType.CONTROLLER, TemplateType.SERVICE, TemplateType.SERVICEIMPL)
-                        .entity("/templates/entity.java") // 禁用模板
-                        .service("/templates/service.java")
-                        .serviceImpl("/templates/serviceImpl.java")
-                        .mapper("/templates/mapper.java")
-                        .mapperXml("/templates/mapper.xml")
-                        .controller("/templates/controller.java")
+//                        .controller("/templates/RestController.java")
                         .build())
 //                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .strategyConfig(builder -> builder
+                        .entityBuilder()
+                        .superClass(BaseEntity.class)
+                        .enableLombok()
+                        .disableSerialVersionUID() // 禁用生成 serialVersionUID
+                        .addIgnoreColumns("id", "createTime", "updateTime")
+                        .build())
                 .execute();
     }
 

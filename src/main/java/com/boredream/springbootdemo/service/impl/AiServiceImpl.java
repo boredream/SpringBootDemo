9827148aa00aacd83d05e3533f2215a9bd2d5e5a
boredream <48262906@qq.com>
2024-09-24 18:45:03 +0800

    file type

--- src/main/java/com/boredream/springbootdemo/service/impl/AiServiceImpl.java
+++ src/main/java/com/boredream/springbootdemo/service/impl/AiServiceImpl.java
@@ -9,6 +9,7 @@ import com.boredream.springbootdemo.service.IAiService;
 import com.boredream.springbootdemo.service.ICaseService;
 import com.boredream.springbootdemo.service.ITalkCaseDetailService;
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
@@ -17,10 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class AiServiceImpl implements IAiService {
 
@@ -81,7 +84,7 @@ public class AiServiceImpl implements IAiService {
             // AI解析失败，更新case状态
             talkCase.setAiParseStatus(Case.AI_PARSE_STATUS_FAIL);
             caseService.updateById(talkCase);
-
+            log.error(e.getMessage());
             throw new ApiException(e.getMessage());
         }
     }
@@ -91,9 +94,21 @@ public class AiServiceImpl implements IAiService {
         System.out.println("case[" + talkCase.getId() + "] sendRequestToAiServer");
         String url = "http://121.41.57.69:80/test";
 
+        // 文本格式：.txt、.doc .docx、.pdf、.rtf、.md
+        // 音频格式：.mp3、.wav .aac、.flac、.ogg、.wma、.m4a
+        int file_type = 0;
+        String fileUrl = talkCase.getFileUrl();
+        if (Arrays.asList(".mp3", ".wav", ".aac", ".flac", ".ogg", ".wma", ".m4a")
+                .contains(fileUrl.substring(fileUrl.lastIndexOf('.')))) {
+            file_type = 1;
+        }
+
+        // TODO 异常没有更新案例错误状态
+        // TODO 假如超时、轮询机制检查案例ai解析状态
+
         Map<String, Object> params = new HashMap<>();
-        params.put("file_type", 0);  // 0为文档，1为音频
-        params.put("doc_url", talkCase.getFileUrl()); // 文档链接
+        params.put("file_type", file_type);  // 0为文档，1为音频
+        params.put("doc_url", fileUrl); // 文档链接
         params.put("counsel", talkCase.getType() == 2 ? 1 : 0);  // 0不是咨询稿件(评估)，1为咨询稿件
 
         long startTime = System.currentTimeMillis();
